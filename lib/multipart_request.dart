334d7f53a95b64cf11f06b0e02c27f5e3b377fa1
JosueMCapita <josuemanuelcapita@gmail.com>
2021-04-29 19:33:54 +0100

    Adding timeout properties to the request

--- lib/multipart_request.dart
+++ lib/multipart_request.dart
@@ -7,7 +7,12 @@ class MultipartRequest {
       const MethodChannel('multipart_request');
 
   String _url;
-  var _headers = {}, _fields = {};
+  var _headers = {}, _fields = {},
+  _timeout = { // in milliseconds
+    "connect": 60 * 1000,
+    "read": 10 * 1000,
+    "write": 10 * 1000
+  };
   var _files = [];
 
   setUrl(String url) {
@@ -33,6 +38,18 @@ class MultipartRequest {
   addFile(key, path) {
     _files.add({"field": key, "path": path});
   }
+  
+  setConnectTimeout(int connectTimeout) {
+    _timeout["connect"] = connectTimeout;
+  }
+  
+  setReadTimeout(int readTimeout) {
+    _timeout["read"] = readTimeout;
+  }
+  
+  setWriteTimeout(int writeTimeout) {
+    _timeout["write"] = writeTimeout;
+  }
 
   Response send() {
     var finalBlock = {
@@ -40,6 +57,7 @@ class MultipartRequest {
       "headers": _headers,
       "fields": _fields,
       "files": _files,
+      "timeout": _timeout
     };
 
     _channel.invokeMethod('multipartRequest', finalBlock);
